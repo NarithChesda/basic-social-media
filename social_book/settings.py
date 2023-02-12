dc0359ed8a7284eb6cc73214732e570717389895
Narith Chesda <narithchesda@gmail.com>
2023-02-13 05:38:49 +0700

    Delete Procfile & Runtime

--- social_book/settings.py
+++ social_book/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m_*l2elcxigy5)12_8)&+ywva!(g%@f$@a)-_7t0hvz3%8ib1@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
