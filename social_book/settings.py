fd048b4d09d8ee51bea3a89bcc0f63c8e0da7027
Narith Chesda <90504714+NarithChesda@users.noreply.github.com>
2023-02-13 14:25:50 +0700

    Update settings.py

--- social_book/settings.py
+++ social_book/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-y6on4&^y1+^c$5_h4eh-i2fz-c6jmagos9$5b!o!%8=08s#!tm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['web-production-a64a.up.railway.app', '127.0.0:1']
 
 
 # Application definition
